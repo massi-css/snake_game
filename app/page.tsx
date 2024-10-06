1d28b9017059937259a0fd5d20cfd2e5ba8d0dcb
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2024-10-06 23:53:34 +0100

    optimized ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { BorderBeam } from "@/components/ui/border-beam";
 import { useCallback, useEffect, useRef, useState } from "react";
+import GridPattern from "@/components/ui/animated-grid-pattern";
+import { cn } from "@/lib/utils";
 
 interface SnakeBody {
   x: number;
@@ -26,14 +29,16 @@ const Home = () => {
         display.style.display = "grid";
         display.style.gridTemplateColumns = `repeat(${numberOfColumns}, 25px)`;
         display.style.gridTemplateRows = `repeat(${numberOfColumns}, 25px)`;
+        display.classList.add("shadow-lg", "rounded-lg");
+        console.log(display.firstChild);
 
-        while (display.firstChild) {
-          display.removeChild(display.firstChild);
-        }
+        // while (display.firstChild) {
+        //   display.removeChild(display.firstChild);
+        // }
 
-        for (let i = 0; i < numberOfColumns * numberOfColumns; i++) {
+        for (let i = 0; i < numberOfColumns * numberOfColumns /2; i++) {
           const cell = document.createElement("div");
-          cell.className = "border border-black";
+          cell.className = "border-[0.2px] border-gray-800 rounded-md";
           cell.style.width = "25px";
           cell.style.height = "25px";
           display.appendChild(cell);
@@ -97,7 +102,7 @@ const Home = () => {
             setFoodExist(false); // Regenerate food
             return newSnake; // Grow snake
           } else {
-            return [newHead, ...prevSnake.slice(0, -1)]; // Move snake
+            return newSnake.slice(0, -1); // Move snake
           }
         });
       };
@@ -112,13 +117,13 @@ const Home = () => {
 
     if (display) {
       Array.from(display.children).forEach((cell) => {
-        (cell as HTMLElement).style.backgroundColor = "white"; // Reset cell color
+        (cell as HTMLElement).style.backgroundColor = "#111827"; // Reset cell color
       });
 
       // Render the snake
       snake.forEach((segment) => {
         const index = segment.y * numberOfColumns + segment.x;
-        if (display.children[index]) {
+        if (display.children[index] && index > 0) {
           (display.children[index] as HTMLElement).style.backgroundColor =
             "green";
         }
@@ -171,22 +176,34 @@ const Home = () => {
   }, [handleDirectionChange]);
 
   return (
-    <div className="size-full flex flex-col space-y-4 items-center justify-center">
+    <div className="size-full relative bg-[#000000] flex flex-col space-y-4 items-center justify-center overflow-hidden">
+      <GridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
       <div
         ref={displayRef}
         id="display"
-        className="w-1/2 h-auto aspect-square"
-      ></div>
+        className="relative h-auto aspect-square bg-gray-900"
+      >
+        <BorderBeam />
+      </div>
       <div
         id="score"
-        className="w-1/2 flex items-center justify-center text-2xl font-bold "
+        className="w-1/2 flex items-center justify-center text-2xl font-bold text-white"
       >
         Score: {score}
       </div>
       <div id="controls" className="flex justify-center space-x-4">
         <button
           id="start"
-          className="bg-blue-500 text-white px-4 py-2"
+          className="bg-green-500 rounded-md text-white px-4 py-2"
           onClick={() => {
             setGameActive(true);
             setScore(0);
@@ -198,34 +215,34 @@ const Home = () => {
         </button>
         <button
           id="stop"
-          className="bg-red-500 text-white px-4 py-2"
+          className="bg-red-500 rounded-md text-white px-4 py-2"
           onClick={() => setGameActive(false)}
         >
           Stop
         </button>
       </div>
-      <div className="flex space-x-2">
+      <div className="lg:hidden flex space-x-2">
         <button
           onClick={() => handleDirectionChange("up")}
-          className="bg-slate-400 px-4 py-2"
+          className="bg-gray-500 rounded-md px-4 py-2"
         >
           Up
         </button>
         <button
           onClick={() => handleDirectionChange("down")}
-          className="bg-slate-400 px-4 py-2"
+          className="bg-gray-500 rounded-md px-4 py-2"
         >
           Down
         </button>
         <button
           onClick={() => handleDirectionChange("left")}
-          className="bg-slate-400 px-4 py-2"
+          className="bg-gray-500 rounded-md px-4 py-2"
         >
           Left
         </button>
         <button
           onClick={() => handleDirectionChange("right")}
-          className="bg-slate-400 px-4 py-2"
+          className="bg-gray-500 rounded-md px-4 py-2"
         >
           Right
         </button>
